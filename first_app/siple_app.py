43d2b8cc9e75207fb099801ce8bdf9feb63243f8
Tarasov Maksim <maksimtarasov1981@gmail.com>
2024-01-16 11:48:29 +0200

    add function add_viking (method POST)

--- first_app/siple_app.py
+++ first_app/siple_app.py
@@ -1,18 +1,29 @@
-
+"""
+Простейшее приложение FastAPI
+"""
 from fastapi import FastAPI
 
 app = FastAPI()
+"""
+Тестовая модель базы данных
+"""
 db = {1: {'name': 'Ivar', 'age': 30},
       2: {'name': 'Bjorn', 'age': 40},
       3: {'name': 'Ragnar', 'age': 50},
       }
+
 @app.get("/")
 def read_root():
+    """
+    Функция фозвращающая ответ за запрос GET, endpoint "/"
+    :return:
+    """
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int,):
-    return {"Viking": db.get(item_id, 'Null'),}
+    return {"Viking": db.get(item_id, 'Null')}
+
 @app.get("/age")
 def read_age(name: str = ''):
     if name:
@@ -21,5 +32,13 @@ def read_age(name: str = ''):
                 return f"{name}, age {db[i]['age']}"
         return f"{name} не найден."
     return [db[key]['name'] for key in db.keys()]
+
+@app.post("/add")
+def add_viking(name: str, age: int):
+    ids = list(db.keys())
+    ids.sort()
+    id = ids[-1] + 1
+    db[id] = {'name': name, 'age': age}
+    return {"Status OK: 200": db[id]}
 
 #uvicorn main:app --reload
